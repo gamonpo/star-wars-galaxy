df095fed60b4f6a6f82a7aab4d9a07be6e99c325
gamonpo <gamonpo@outlook.com>
2020-05-16 13:42:31 -0300

    Screenshot

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import {StatusBar} from 'react-native';
 import Src from './src';
 
 const planet_svg = () => {
-  return <Src />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#1a1a1a" />
+      <Src />
+    </>
+  );
 };
 
 export default planet_svg;
